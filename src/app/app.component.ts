f980cb0c36d52021c5d4bf195df8ab32a32d58b4
Emma781227 <emmaengongomo5@gmail.com>
2024-03-28 14:36:50 +0100

    Debut du disgn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { POKEMONS } from './mock-pokemon-listes';
+import {Pokemon} from './pokemon';
+import { CommonModule } from '@angular/common';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [RouterOutlet,  CommonModule],
+  templateUrl:'app.component.html' 
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon [] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+    // this.selectPokemon(this.pokemonList[3]);
+  }
+  // constructor(private dialog: MatDialog) {}
+
+  // méthodeQuiSignaleErreur() {
+  //   const dialogConfig = new MatDialogConfig();
+  
+  //   // Configuration pour afficher la popup en haut de l'écran
+  //   dialogConfig.position = {
+  //     top: '0',
+  //     right: '0'
+  //   };
+  selectPokemon(pokemonId: string){
+    const pokemon: Pokemon|undefined  = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    if (pokemon)  {
+      // Afficher le nom du Pokémon correspondant à l'index
+      console.log(`Vous avez cliqué sur le Pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else{
+      console.log(`Vous avec cliquer sur un pokemon qui nexist pas`);
+      this.pokemonSelected = pokemon;
+    }
+ 
+  }
 }
+ 
+  
---
+++ src/app/error-dialog/error-dialog.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-dialog',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      error-dialog works!
+    </p>
+  `,
+  styles: ``
+})
+export class ErrorDialogComponent {
+
+}
